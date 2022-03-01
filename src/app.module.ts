679de7ac1ab27dbafb632fb98dc31d07f095080b
p-Jimenez <pablo.jimg@gmail.com>
2022-03-01 14:54:21 +0100

    Added root endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 
+import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -13,5 +14,6 @@ import { PrismaModule } from './prisma/prisma.module';
     AuthModule, 
     UserModule, 
     PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule {}
